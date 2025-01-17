b9447bd49e4d16625a8f6348e64ceb688981aabc
thenagarajanv <thenagarajanv@gmail.com>
2025-01-17 21:13:49 +0530

    resolved bug

--- src/app/(authentications)/auth/reset-password/page.js
+++ src/app/(authentications)/auth/reset-password/page.js
@@ -1,4 +1,6 @@
 import Resetpassword from '@/app/_components/auth/Reset-Password/ResetPassword.jsx';
+import { Suspense } from 'react';
+
 export async function generateMetadata() {
   const title = 'Reset Password :: Splitwise';
   const describe = 'Splitwise Reset Password';
@@ -12,7 +14,9 @@ export async function generateMetadata() {
 const ResetPassword = () => {
   return <div>
     <div className="md-3">
+    <Suspense fallback={<div>Loading...</div>}>
       <Resetpassword/>
+    </Suspense>
     </div>
   </div>;
 };
