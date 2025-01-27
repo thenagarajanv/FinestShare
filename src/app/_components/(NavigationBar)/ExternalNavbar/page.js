3064d844261d2aa96042157bed7f5e9886992cd0
thenagarajanv <thenagarajanv@gmail.com>
2025-01-27 16:29:20 +0530

    final

--- src/app/_components/(NavigationBar)/ExternalNavbar/page.js
+++ src/app/_components/(NavigationBar)/ExternalNavbar/page.js
@@ -35,13 +35,12 @@ const ExternalNavbar = () => {
           onMouseEnter={() => setIsHovering(true)} 
           onMouseLeave={() => setIsHovering(false)} 
         >
-            <div className="flex font-basement align-middle items-center text-center uppercase justify-center text-white  text-xl font-extrabold">
+            <div  onClick={() => router.push("/")} className="flex font-basement align-middle items-center text-center uppercase justify-center text-white  text-xl font-extrabold">
               Finest
             <img
             id="logo"
             src="/img/pnglogo.png"
             className={` h-[50px] cursor-pointer`}
-            onClick={() => router.push("/")}
             alt="Logo"
             />
             SHARE
--- src/app/_components/(NavigationBar)/InternalNavbar/page.js
+++ src/app/_components/(NavigationBar)/InternalNavbar/page.js
@@ -63,7 +63,7 @@ const InternalNavbar = () => {
           onMouseEnter={() => setIsHovering(true)} 
           onMouseLeave={() => setIsHovering(false)} 
         >
-          <div className="flex align-middle items-center leading-0   text-center uppercase justify-center text-white  text-xl font-extrabold">
+          <div  onClick={() => router.push("/")} className="flex align-middle items-center leading-0   text-center uppercase justify-center text-white  text-xl font-extrabold">
               Finest
             <Image
             width={200}
